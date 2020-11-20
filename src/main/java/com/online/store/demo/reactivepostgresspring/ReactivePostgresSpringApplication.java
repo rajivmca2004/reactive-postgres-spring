ff6d31f069c17f023314df748e8ce8201044bd98
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-11-20 17:02:11 +0530

    first commit

---
+++ src/main/java/com/online/store/demo/reactivepostgresspring/ReactivePostgresSpringApplication.java
@@ -0,0 +1,13 @@
+package com.online.store.demo.reactivepostgresspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactivePostgresSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactivePostgresSpringApplication.class, args);
+	}
+
+}
