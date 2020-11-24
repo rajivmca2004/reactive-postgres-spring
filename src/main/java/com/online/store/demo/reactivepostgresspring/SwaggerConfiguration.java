fe325fff6b7ea6c5ac81bd2bffcf6b3a6de66d6e
Rajiv Srivastava <rajivmca2004@gmail.com>
2020-11-25 01:35:47 +0530

    Added new code for Mono and Flux

---
+++ src/main/java/com/online/store/demo/reactivepostgresspring/SwaggerConfiguration.java
@@ -0,0 +1,34 @@
+package com.online.store.demo.reactivepostgresspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.config.WebFluxConfigurer;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration implements WebFluxConfigurer {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .enable(true)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder().title("Spring Reactive Stream Demo")
+                                    .description("Reactive Stream Demo")
+                                    .version("1.0").build();
+    }
+}
